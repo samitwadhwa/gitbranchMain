17e8ef94e5d2853dc7ac7cbed98bbe56c0ad74d1
samitwadhwa <samitwadhwa@gmail.com>
2025-09-16 14:38:59 +0530

    main change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     Hello ! Main branch
+     Hello I am ! Main branch
+     nd please keep my changes before pushing any further change 
     </div>
   );
 }
